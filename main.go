1c57cc971c5f693a0e8a1f04d49ac6507054eaf9
ganso-kyamada <thisismystory.1212@gmail.com>
2022-09-05 09:23:41 +0900

    csvから予約できるように修正

--- main.go
+++ main.go
@@ -1,17 +1,82 @@
 package main
 
 import (
-	"flag"
+	"encoding/csv"
 	"fmt"
 	"os"
 )
 
-func main() {
-	uid := flag.String("i", "userid", "login user id")
-	pass := flag.String("p", "password", "login password")
-	url := os.Getenv("URL")
-	flag.Parse()
+type Schedule struct {
+	Date   string
+	Start  string
+	End    string
+	People string
+	Places []string
+}
 
-	fmt.Printf("UserId: %s, Password: %s, URL: %s\n", *uid, *pass, url)
-	Reservation(*uid, *pass, url)
+type User struct {
+	ID         string
+	Password   string
+	SecurityNo string
+	Url        string
+	Schedules  []Schedule
+}
+
+func main() {
+	url := os.Getenv("URL")
+
+	schedulesFile, err := os.Open("schedules.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer schedulesFile.Close()
+
+	schedulesFileReader := csv.NewReader(schedulesFile)
+	schedulesRows, err := schedulesFileReader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	usersFile, err := os.Open("users.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer usersFile.Close()
+
+	usersFileReader := csv.NewReader(usersFile)
+	usersRows, err := usersFileReader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i, u := range usersRows {
+		if i == 0 {
+			continue
+		}
+
+		var schedules []Schedule
+		for j, s := range schedulesRows {
+			if j == 0 {
+				continue
+			}
+			schedule := Schedule{
+				Date:   s[0],
+				Start:  s[1],
+				End:    s[2],
+				People: s[3],
+				Places: s[4:],
+			}
+			schedules = append(schedules, schedule)
+		}
+
+		user := User{
+			ID:         u[0],
+			Password:   u[1],
+			SecurityNo: u[2],
+			Url:        url,
+			Schedules:  schedules,
+		}
+
+		Reservation(user)
+	}
 }
--- scrape.go
+++ scrape.go
@@ -16,16 +16,17 @@ type Path struct {
 	Complete      string
 }
 
-func Reservation(uid string, pass string, url string) {
+func Reservation(user User) {
+	num := 0
 	first := true
 	path := Path{
-		Login:         url + "/rsvWUserAttestationAction.do",
-		Menu:          url + "/lotWTransLotAcceptListAction.do",
-		Lottery:       url + "/lotWTransLotBldGrpAction.do",
-		GroundMenu:    url + "/lotWTransLotInstGrpAction.do",
-		GroundNumMenu: url + "/lotWTransLotInstSrchVacantAction.do",
-		Apply:         url + "/lotWInstTempLotApplyAction.do",
-		Complete:      url + "/lotWInstLotApplyAction.do",
+		Login:         user.Url + "/rsvWUserAttestationAction.do",
+		Menu:          user.Url + "/lotWTransLotAcceptListAction.do",
+		Lottery:       user.Url + "/lotWTransLotBldGrpAction.do",
+		GroundMenu:    user.Url + "/lotWTransLotInstGrpAction.do",
+		GroundNumMenu: user.Url + "/lotWTransLotInstSrchVacantAction.do",
+		Apply:         user.Url + "/lotWInstTempLotApplyAction.do",
+		Complete:      user.Url + "/lotWInstLotApplyAction.do",
 	}
 
 	c := colly.NewCollector()
@@ -34,84 +35,124 @@ func Reservation(uid string, pass string, url string) {
 			return
 		}
 
-		fmt.Printf("UID[%s] Login...\n", uid)
+		fmt.Printf("UID[%s] Login...\n", user.ID)
 		first = false
 		err := c.Post(path.Login, map[string]string{
-			"userId":    uid,
-			"password":  pass,
-			"displayNo": "pawab2000",
-			"loginJKey": e.Attr("value"),
+			"userId":     user.ID,
+			"password":   user.Password,
+			"displayNo":  "pawab2000",
+			"securityNo": user.SecurityNo,
+			"loginJKey":  e.Attr("value"),
 		})
 		if err != nil {
-			fmt.Printf("UID[%s] Login Error!!! %s\n", uid, err)
+			fmt.Printf("UID[%s] Login Error!!! %s\n", user.ID, err)
 		}
 	})
 
 	c.OnHTML("input.logoutbtn", func(e *colly.HTMLElement) {
 		switch e.Request.URL.String() {
 		case path.Login:
-			fmt.Printf("UID[%s] Login suceess!!\n", uid)
-			c.Post(path.Menu, map[string]string{
-				"displayNo":       "plwac3000",
-				"selectPpsPpsdCd": "100",
-				"selectPpsCd":     "100130",
-			})
+			fmt.Printf("UID[%s] Login suceess!!\n", user.ID)
+			visitMenuPage(c, path.Menu)
 		case path.Menu:
-			fmt.Printf("UID[%s] Menu\n", uid)
-			c.Post(path.Lottery, map[string]string{
-				"displayNo":      "plwad1000",
-				"selectClassCd": "5060010",
-			})
+			fmt.Printf("UID[%s] Menu\n", user.ID)
+			visitLotteryPage(c, path.Lottery)
 		case path.Lottery:
-			fmt.Printf("UID[%s] Lottery\n", uid)
-			c.Post(path.GroundMenu, map[string]string{
-				"displayNo":      "plwba1000",
-				"selectBldGrpCd": "5450010",
-			})
-			// INFO: 複数のグラウンドがあった場合（第一運動場、第二運動場など）
-			// err := c.Post(path.GroundNumMenu, map[string]string{
-			// 	"displayNo":      "plwba2000",
-			// 	"selectBldGrpCd": "5110030",
-			// 	"selectUseNumber": "50", // ここの値は固定値か要確認
-			// })
-		case path.GroundMenu, path.GroundNumMenu:
-			fmt.Printf("UID[%s] GroundMenu\n", uid)
-			c.Post(path.Apply, map[string]string{
-				"selectFieldCnt":  "1",
-				"displayNo":       "plwba3000",
-				"selectUseYMD":    "20221002",
-				"selectTimeNum":   "1",
-				"maxFieldCnt":     "1",
-				"selectDispStime": "800",
-				"selectDispEtime": "1000",
-				"dummy":           "",
-				"dummy2":          "",
-			})
+			fmt.Printf("UID[%s] Lottery\n", user.ID)
+			visitGroundMenu(c, path.GroundMenu, user.Schedules[num])
+		case path.GroundMenu:
+			fmt.Printf("UID[%s] GroundMenu\n", user.ID)
+			schedule := user.Schedules[num]
+			if len(schedule.Places) == 2 && schedule.Places[1] != "" {
+				visitGroundNumMenu(c, path.GroundNumMenu, schedule)
+			} else {
+				visitApply(c, path.Apply, schedule)
+			}
+		case path.GroundNumMenu:
+			fmt.Printf("UID[%s] GroundNumMenu\n", user.ID)
+			visitApply(c, path.Apply, user.Schedules[num])
 		case path.Complete:
-			fmt.Printf("UID[%s] Complete!!\n", uid)
+			fmt.Printf("UID[%s] Complete!!\n", user.ID)
+			if len(user.Schedules) > num+1 {
+				num += 1
+				visitMenuPage(c, path.Menu)
+			}
 		}
 	})
 
 	c.OnHTML("input[name=insLotJKey]", func(e *colly.HTMLElement) {
-		fmt.Printf("exists insLotJKey %s\n", e.Request.URL.String())
-		c.Post(path.Complete, map[string]string{
-			"displayNo":       "plwca1000",
-			"applyPepopleNum": "15", // 人数
-			"selectApplyNo":   "-1",
-			"selectHopeNo":    "1",
-			"selectPpsName":   "%8F%AD%94N%96%EC%8B%85%81i%8F%AC%81E%92%86%8Aw%90%B6%81j",
-			"insLotJKey": e.Attr("value"),
-			"dummy": "",
-		})
+		if len(user.Schedules) <= num+1 {
+			return
+		}
+
+		fmt.Printf("UID[%s] exists insLotJKey %s\n", user.ID, e.Request.URL.String())
+		visitComplete(c, path.Complete, user.Schedules[num], e.Attr("value"))
 	})
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Printf("UID[%s] Request %s\n", uid, r.URL)
+		fmt.Printf("UID[%s] Request %s\n", user.ID, r.URL)
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		fmt.Printf("UID[%s] Response code %d\n", uid, r.StatusCode)
+		fmt.Printf("UID[%s] Response code %d\n", user.ID, r.StatusCode)
 	})
 
-	c.Visit(url)
+	c.Visit(user.Url)
+}
+
+func visitMenuPage(c *colly.Collector, path string) {
+	c.Post(path, map[string]string{
+		"displayNo":       "plwac3000",
+		"selectPpsPpsdCd": "100",
+		"selectPpsCd":     "100130",
+	})
+}
+
+func visitLotteryPage(c *colly.Collector, path string) {
+	c.Post(path, map[string]string{
+		"displayNo":     "plwad1000",
+		"selectClassCd": "5060010",
+	})
+}
+
+func visitGroundMenu(c *colly.Collector, path string, schedule Schedule) {
+	c.Post(path, map[string]string{
+		"displayNo":      "plwba1000",
+		"selectBldGrpCd": schedule.Places[0],
+	})
+}
+
+func visitGroundNumMenu(c *colly.Collector, path string, schedule Schedule) {
+	// INFO: 複数のグラウンドがあった場合（第一運動場、第二運動場など）
+	c.Post(path, map[string]string{
+		"displayNo":       "plwba2000",
+		"selectBldGrpCd":  schedule.Places[1],
+		"selectUseNumber": "50", // ここの値は固定値か要確認
+	})
+}
+
+func visitApply(c *colly.Collector, path string, schedule Schedule) {
+	c.Post(path, map[string]string{
+		"selectFieldCnt":  "1",
+		"displayNo":       "plwba3000",
+		"selectUseYMD":    schedule.Date,
+		"selectTimeNum":   "1",
+		"maxFieldCnt":     "1",
+		"selectDispStime": schedule.Start,
+		"selectDispEtime": schedule.End,
+		"dummy":           "",
+		"dummy2":          "",
+	})
+}
+
+func visitComplete(c *colly.Collector, path string, schedule Schedule, insLotJKey string) {
+	c.Post(path, map[string]string{
+		"displayNo":       "plwca1000",
+		"applyPepopleNum": schedule.People,
+		"selectApplyNo":   "-1",
+		"selectHopeNo":    "1",
+		"selectPpsName":   "%8F%AD%94N%96%EC%8B%85%81i%8F%AC%81E%92%86%8Aw%90%B6%81j",
+		"insLotJKey":      insLotJKey,
+		"dummy":           "",
+	})
 }
